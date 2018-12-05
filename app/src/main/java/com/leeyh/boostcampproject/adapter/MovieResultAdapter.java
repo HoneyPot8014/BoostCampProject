c7d9415f07254b8d3077dedd853f51093aecf98e
leeyh <gch02310@naver.com>
2018-12-05 16:04:15 +0900

    main ui update

---
+++ app/src/main/java/com/leeyh/boostcampproject/adapter/MovieResultAdapter.java
@@ -0,0 +1,12 @@
+package com.leeyh.boostcampproject.adapter;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+public class MovieResultAdapter extends RecyclerView.ViewHolder<> {
+
+    public MovieResultAdapter(@NonNull View itemView) {
+        super(itemView);
+    }
+}
